7577409ca43a932021dba4254efd844e031e7bd2
Rinaldots <rinaldotavares12@gmail.com>
2024-08-17 14:34:53 -0300

    Add files via upload
    fist commit - can simulate d4nc3r bot in gazebo world

---
+++ d4nc3r_gz/launch/d4nc3r_one_robot.launch.py
@@ -0,0 +1,113 @@
+# Licensed under the 3-Clause BSD License
+#
+# Copyright (c) 2023, Ekumen Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""Launch Gazebo with a world that has Andino."""
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Arguments
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_rviz = LaunchConfiguration('rviz')
+    rviz_config_file = LaunchConfiguration('rviz_config_file')
+
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_d4nc3r_gz = get_package_share_directory('d4nc3r_gz')
+
+    use_sim_time_argument = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true',
+    )
+
+    world_argument = DeclareLaunchArgument(
+        name='world',
+        default_value='empty.world',
+        description='Full path to the world model file to load')
+
+    use_rviz_argument = DeclareLaunchArgument(
+        'rviz', default_value='true', description='Open RViz.'
+    )
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        'rviz_config_file',
+        default_value=os.path.join(
+            pkg_d4nc3r_gz, 'rviz', 'd4nc3r_gz.rviz'),
+        description='Full path to the RVIZ config file to use',
+    )
+
+    # Include andino
+    include_d4nc3r = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_d4nc3r_gz, 'launch', 'spawn_robot.launch.py')
+        ),
+    )
+
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
+        ),
+        launch_arguments={
+            'world': LaunchConfiguration('world'),
+        }.items()
+    )
+
+    # RViz
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        parameters=[{'use_sim_time': use_sim_time}],
+        arguments=['-d', rviz_config_file],
+    )
+
+    d4nc3r_visualization_timer = TimerAction(
+        period=5.0, actions=[rviz], condition=IfCondition(use_rviz)
+    )
+    return LaunchDescription(
+        [
+            use_sim_time_argument,
+            declare_rviz_config_file_cmd,
+            world_argument,
+            use_rviz_argument,
+            gazebo,
+            include_d4nc3r,
+            d4nc3r_visualization_timer,
+        ]
+    )
