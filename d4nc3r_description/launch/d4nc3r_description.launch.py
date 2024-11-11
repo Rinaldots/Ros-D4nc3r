da1aad204bce8d0cfdad8079ef4dffc9f66f3cdd
rinaldots <rinaldotavares12@gmail.com>
2024-11-11 19:18:19 -0300

    ok

--- d4nc3r_description/launch/d4nc3r_description.launch.py
+++ d4nc3r_description/launch/d4nc3r_description.launch.py
@@ -11,36 +11,33 @@ import xacro
 
 def generate_launch_description():
     
-    use_sim_time = LaunchConfiguration('use_sim_time')
     # Arguments
     rsp_argument = DeclareLaunchArgument('rsp', default_value='true',
                           description='Run robot state publisher node.')
-
+    namespace_arg = DeclareLaunchArgument('namespace', default_value='d4nc3r1', description='Default namespace')
+    namespace = LaunchConfiguration('namespace')
     # Obtains d4nc3r_description's share directory path.
     pkg_d4nc3r_description = get_package_share_directory('d4nc3r_description')
 
     # Obtain urdf from xacro files.
     arguments = {'yaml_config_dir': os.path.join(pkg_d4nc3r_description, 'config', 'd4nc3r')}
-    doc = xacro.process_file(os.path.join(pkg_d4nc3r_description, 'urdf', 'd4nc3r.urdf'), mappings = arguments)
+    doc = xacro.process_file(os.path.join(pkg_d4nc3r_description, 'urdf', 'd4nc3r.urdf.xacro'), mappings = arguments)
     robot_desc = doc.toprettyxml(indent='  ')
     params = {'robot_description': robot_desc,
-              'publish_frequency': 30.0,
-              'use_sim_time': use_sim_time}
+              'publish_frequency': 30.0}
 
     # Robot state publisher
     rsp = Node(package='robot_state_publisher',
                 executable='robot_state_publisher',
-                namespace='',
+                namespace=namespace,
                 output='screen',
                 parameters=[params],
                 condition=IfCondition(LaunchConfiguration('rsp'))
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
+        
+        namespace_arg,
         rsp_argument,
         rsp,
     ])
